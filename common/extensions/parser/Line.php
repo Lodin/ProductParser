5e288953a2990fbffd3b38877eb863b6da1331d9
Vlad Rindevich <rindevich.vs@gmail.com>
2015-09-16 23:50:17 +0600

    Improve parser components

--- common/extensions/parser/Line.php
+++ common/extensions/parser/Line.php
@@ -6,6 +6,7 @@ class Line
 {
     protected $_original = '';
     protected $_words = [];
+    protected $_isStick = false;
     
     protected function __construct() {}
     
@@ -14,8 +15,23 @@ class Line
         $line = new Line;
         
         $line->_original = $data;
-        foreach (explode(' ', $data) as $word) {
-            $this->_words[] = Word::from($word);
+        foreach ($line->split($data) as $word) {
+            if ($line->hasMultipleColors($word)) {
+                $line->handleMultipleColors($word);
+                continue;
+            }
+            
+            if ($this->isStick($word)) {
+                $this->_isStick = true;
+            }
+            
+            $word = Word::from($word);
+            
+            if ($this->_isStick && Word::hasStickOrientation($word)) {
+                $word->asStickOrientation();
+            }
+        
+            $line->_words[] = $word;
         }
         
         return $line;
@@ -44,7 +60,19 @@ class Line
     
     public function apply(Product $product)
     {
+        $list = [
+            Word::TYPE_ARTICLE => '',
+            Word::TYPE_NAME_PART => '',
+            Word::TYPE_SIZE_PART => '',
+            Word::TYPE_MODEL_PART => '',
+            Word::TYPE_BRAND => '',
+            Word::TYPE_COLOR => '',
+            Word::TYPE_STICK_ORIENTATION => ''
+        ];
         
+        foreach ($this->_words as $word) {
+            $word->attach($list);
+        }
     }
     
     public function count()
@@ -71,5 +99,32 @@ class Line
         } else {
             return [$first, $second];
         }
+    }
+    
+    protected function hasMultipleColors($str)
+    {
+        return strpos($str, '\\') !== false && Word::hasColor($str);
+    }
+    
+    protected function handleMultupleColors($str)
+    {
+        foreach ($this->splitColors($str) as $word) {
+            $this->_words[] = Word::from($word)->asColor();
+        }
+    }
+    
+    protected function split($str)
+    {
+        return explode(' ', $str);
+    }
+    
+    protected function splitColors($str)
+    {
+        return explode('\\', $str);
+    }
+    
+    protected function isStick($word)
+    {
+        return (bool)preg_match('/клюшк/i', $word);
     }
 }
--- common/extensions/parser/Word.php
+++ common/extensions/parser/Word.php
@@ -7,6 +7,8 @@ class Word
     const TYPE_SIZE_PART = 2;
     const TYPE_MODEL_PART = 3;
     const TYPE_BRAND = 4;
+    const TYPE_COLOR = 5;
+    const TYPE_STICK_ORIENTATION = 6;
     
     protected $_basic;
     protected $_original;
@@ -24,12 +26,37 @@ class Word
         $word->_original = trim($data);
         
         if ($word->_basic === null) {
-            $this->_empty = true;
+            $word->_empty = true;
         } else {
-            $this->test();
+            $word->test();
         }
         
         return $word;
+    }
+    
+    public static function hasArticle($word)
+    {
+        return preg_match('/\w*?\d{6,}/', static::check($word));
+    }
+    
+    public static function hasNamePart($word)
+    {
+        return preg_match('/[А-Яа-я]+/', static::check($word));
+    }
+    
+    public static function hasSizePart($word)
+    {
+        return preg_match('/euro|pant|jr|sr|yth|s|m|l|xl|xxl|\d{0,3}\b/ig', static::check($word));
+    }
+    
+    public static function hasColor($word)
+    {
+        return preg_match('/крас|черн|бел|золот|син|желт/i', static::check($word));
+    }
+    
+    public static function hasStickOrientation($word)
+    {
+        return preg_match('/l|r/i', static::check($word));
     }
     
     public function countUp(array &$count)
@@ -44,6 +71,16 @@ class Word
     public function peek(array $count)
     {
         return $count[$this->_basic];
+    }
+    
+    public function attach(array &$list)
+    {
+        switch ($this->_type) {
+            case self::TYPE_NAME_PART:
+            case self::TYPE_SIZE_PART:
+            case self::TYPE_MODEL_PART:
+                $list[$this->_type] .= " {$this->_original}"
+        }
     }
     
     public function isEmpty()
@@ -66,10 +103,16 @@ class Word
         return $this->_type === self::TYPE_SIZE_PART;
     }
     
+    public function isColorPart()
+    {
+        return $this->_type === self::TYPE_COLOR;
+    }
+    
     public function isUnknownPart()
     {
         return !$this->isArticle() && !$this->isNamePart()
-            && !$this->isSizePart();
+            && !$this->isSizePart() && !$this->isColorPart()
+            && !$this->isStickOrientation();
     }
     
     public function isBrand()
@@ -82,31 +125,67 @@ class Word
         return $this->_type === self::TYPE_MODEL_PART;
     }
     
+    public function isStickOrientation()
+    {
+        return $this->_type === self::TYPE_STICK_ORIENTATION;
+    }
+    
+    public function asStickOrientation()
+    {
+        $this->_type = self::TYPE_STICK_ORIENTATION;
+        return $this;
+    }
+    
+    public function asColor()
+    {
+        $this->_type = self::TYPE_COLOR;
+        return $this;
+    }
+    
     public function asBrand()
     {
         $this->_type = self::TYPE_BRAND;
+        return $this;
     }
     
     public function asModelPart()
     {
         $this->_type = self::TYPE_MODEL_PART;
+        return $this;
     }
     
     protected static function clean($word)
     {
         $pocket = [];
-        preg_match('/\w+/ig', strtolower(trim($word)), $pocket);
+        preg_match('/\w+/ig', strtolower(trim($word)), $pocket); // TODO
         return isset($pocket[0])? $pocket[0] : null;
+    }
+    
+    protected static function check($word)
+    {
+        if ($word instanceof Word) {
+            return $word->_basic;
+        } elseif (is_string($word)) {
+            return $word;
+        }
+        
+        throw new LogicException('$word should be string or Word instance');
     }
     
     protected function test()
     {
-        if (preg_match('/\w*?\d{6,}/', $this->_basic)) {
+        if (!empty($this->_type)) {
+            return;
+        }
+        
+        if (static::hasArticle($this->_basic)) {
             $this->_type = self::TYPE_ARTICLE;
-        } elseif (preg_match('/[А-Яа-я]+/', $this->_basic)) {
+        } elseif (static::hasNamePart($this->_basic)) {
             $this->_type = self::TYPE_NAME_PART;
-        } elseif (preg_match('/euro|pant|jr|sr|yth|s|m|l|xl|xxl|\d{0,3}\b/ig',$this->_basic)) {
+        } elseif (static::hasSizePart($this->_basic)) {
             $this->_type = self::TYPE_SIZE_PART;
+        } elseif (static::hasColor($this->_basic)) {
+            $this->_type = self::TYPE_COLOR;
         }
     }
 }
